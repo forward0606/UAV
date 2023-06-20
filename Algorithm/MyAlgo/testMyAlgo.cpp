52b14a6c5bdaba607828b5e212f0449e5f04550d
forward0606 <chen910606@gmail.com>
2023-06-21 02:52:09 +0800

    add run time measured

--- Algorithm/MyAlgo/testMyAlgo.cpp
+++ Algorithm/MyAlgo/testMyAlgo.cpp
@@ -1,7 +1,11 @@
 #include "MyAlgo.h"
+#include <sys/time.h>
 using namespace std;
 
 int main(){
+    timeval start;
+    timeval end;
+    gettimeofday(&start, NULL);
     Input input(5);
     input.display();
     MyAlgo algo(input);
@@ -9,5 +13,8 @@ int main(){
     // algo.display();
     algo.make_tree();
     algo.tree_travse();
+    algo.get_dp_table();
     cout << "tsp ans: " << algo.get_dp_table().begin()->second << endl;
+    gettimeofday(&end, NULL);
+    cout<<"run time: "<<(1000000LL * (end.tv_sec - start.tv_sec) + end.tv_usec - start.tv_usec) / 1000000.0<<"sec"<<endl;
 }
