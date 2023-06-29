7309125ee933a96383aed85ac1a1a22a65bdd6d8
forward0606 <chen910606@gmail.com>
2023-06-29 23:10:35 +0800

    update parameter

--- Algorithm/MyAlgo/Parameter.h
+++ Algorithm/MyAlgo/Parameter.h
@@ -8,7 +8,7 @@ public:
     static constexpr double epsilon = 5.0;
     static constexpr double epsilon_plum = 2.0;
     static constexpr double k = 4;
-    static constexpr double random_keep_ratio = 0.2;
+    static constexpr double random_keep_ratio = 0.01;
 };
 
 #endif
